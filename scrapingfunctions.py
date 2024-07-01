afe6121f480cd9867dbc1f94a32ffe24014ae049
thegigacoder123 <muaazarkhan@gmail.com>
2024-07-01 10:20:08 +0500

    added a 404 error handler and a element not found error handler

--- scrapingfunctions.py
+++ scrapingfunctions.py
@@ -1,6 +1,8 @@
 import requests as requ
 from bs4 import BeautifulSoup as bs
 
+from urllib.error import *
+
 import os
 
 import threading
@@ -9,7 +11,8 @@ import random
 import datetime
 
 
-# A custom thread class that incorporates returning a value since the original one doesnt
+
+# A custom thread class that incorporates returning a value since the original one doesnt (currently not using the return so :\ )
 class CustomThread(threading.Thread):
     
     def __init__(self, group=None, target=None, name=None,
@@ -146,7 +149,9 @@ def request_executor(url, params_list:dict, elems_list, req_type):
             except ConnectionError:
                 # log the connection error
                 error_logger(url=url, time=f"Start Time: {start_time}   End Time: {str(datetime.datetime.now())}", status='ERROR', error='Connection Error', request_type=req_type)
-
+            
+            except HTTPError:
+                error_logger(url=url, time=f"Start Time: {start_time}   End Time: {str(datetime.datetime.now())}", status='ERROR', error='Site not found Error', request_type=req_type)
                 # return the output for a user on the GUI f"Start Time: {start_time} End Time: {str(datetime.datetime.now())}"
                 #return [str(datetime.datetime.now()), url, 'ERROR', v_o_params_list, 'Connection Error', 'GET', '-', 'error scrape']
 
@@ -182,10 +187,14 @@ def request_executor(url, params_list:dict, elems_list, req_type):
 
                     for x in elems:
                         element_scraped = page_soup.find_all(x['name'], {x['attribute']: x['attribute value']})
+                        
                         for i in element_scraped:
-                            f_a.write(f'{str(i.text).strip()}\n')
-                            elems_logging_list.append(f"[{req_type}] [{code}] [Start Time: {start_time}  End Time: {str(datetime.datetime.now())}]  url={url}  elements={x['name']}  parameters={params_list}\n")
-
+                            # handling html tags not found errors.
+                            try:
+                                f_a.write(f'{str(i.text).strip()}\n')
+                                elems_logging_list.append(f"[{req_type}] [{code}] [Start Time: {start_time}  End Time: {str(datetime.datetime.now())}]  url={url}  elements={x['name']}  parameters={params_list}\n")
+                            except AttributeError:
+                                error_logger(url=url, time=f"Start Time: {start_time}   End Time: {str(datetime.datetime.now())}", status='ERROR', error=f'{i} was not found on the webpage', request_type=req_type)
                     
                 element_logger(elements_logs_list=elems_logging_list)
                             
@@ -201,7 +210,8 @@ def request_executor(url, params_list:dict, elems_list, req_type):
                 req = req.content
             except ConnectionError:
                 error_logger(url=url, time=f"Start Time: {start_time}   End Time: {str(datetime.datetime.now())}", status='ERROR', error='Connection Error', request_type=req_type)
-
+            except HTTPError:
+                error_logger(url=url, time=f"Start Time: {start_time}   End Time: {str(datetime.datetime.now())}", status='ERROR', error='Site not found Error', request_type=req_type)
                 # return the output for a user on the GUI
                 #return [str(datetime.datetime.now()), url, 'ERROR', v_o_params_list, 'Connection Error', 'GET', '-', 'error scrape']
 
@@ -228,19 +238,22 @@ def request_executor(url, params_list:dict, elems_list, req_type):
                 
             else:
                 elems = elems_list['elements']
-
                 save_path = f'{curr_dir}\\data\\scraped-data\\{str(datetime.datetime.now()).split('.')[0].replace(" ", '_').replace(':', '-')}--{str(url).replace('/', '=').replace('.', '-').replace(':', '')}-elements_scraped.txt'
-                
+
+                # open the save file and assign it a unique name
                 with open(save_path, 'a', errors='ignore') as f_a:
                     elems_logging_list = []
+
                     for x in elems:
-
                         element_scraped = page_soup.find_all(x['name'], {x['attribute']: x['attribute value']})
-
+                        
                         for i in element_scraped:
-                            f_a.write(f'{str(i.text).strip()}\n')
-                            elems_logging_list.append(f"[{req_type}] [{code}] [Start Time: {start_time}  End Time: {str(datetime.datetime.now())}]  url={url}  elements={x['name']}  parameters={params_list}\n")
-
+                            # handling html tags not found errors.
+                            try:
+                                f_a.write(f'{str(i.text).strip()}\n')
+                                elems_logging_list.append(f"[{req_type}] [{code}] [Start Time: {start_time}  End Time: {str(datetime.datetime.now())}]  url={url}  elements={x['name']}  parameters={params_list}\n")
+                            except AttributeError:
+                                error_logger(url=url, time=f"Start Time: {start_time}   End Time: {str(datetime.datetime.now())}", status='ERROR', error=f'{i} was not found on the webpage', request_type=req_type)
                     
                 element_logger(elements_logs_list=elems_logging_list)
 
@@ -258,9 +271,11 @@ def request_executor(url, params_list:dict, elems_list, req_type):
         except ConnectionError:
             # log the connection error
             error_logger(url=url, time=f"Start Time: {start_time}   End Time: {str(datetime.datetime.now())}", status='ERROR', error='Connection Error', request_type=req_type)
-
+        except HTTPError:
+            # log the 404 error, if the site returns it
+            error_logger(url=url, time=f"Start Time: {start_time}   End Time: {str(datetime.datetime.now())}", status='ERROR', error='Site not found Error', request_type=req_type)
             # return the output for a user on the GUI
-            return [str(datetime.datetime.now()), url, 'ERROR', v_o_params_list, 'Connection Error', 'GET', '-', 'error scrape']
+            # return [str(datetime.datetime.now()), url, 'ERROR', v_o_params_list, 'Connection Error', 'GET', '-', 'error scrape']
 
         page_soup = bs(req, features='html.parser')
         scraped_page = page_soup.prettify()
@@ -286,14 +301,20 @@ def request_executor(url, params_list:dict, elems_list, req_type):
             elems = elems_list['elements']
             save_path = f'{curr_dir}\\data\\scraped-data\\{str(datetime.datetime.now()).split('.')[0].replace(" ", '_').replace(':', '-')}--{str(url).replace('/', '=').replace('.', '-').replace(':', '')}-elements_scraped.txt'
 
+                # open the save file and assign it a unique name
             with open(save_path, 'a', errors='ignore') as f_a:
                 elems_logging_list = []
+
                 for x in elems:
                     element_scraped = page_soup.find_all(x['name'], {x['attribute']: x['attribute value']})
+                        
                     for i in element_scraped:
-                        f_a.write(f'{str(i.text).strip()}\n')
-                        elems_logging_list.append(f"[{req_type}] [{code}] [Start Time: {start_time}  End Time: {str(datetime.datetime.now())}]  url={url}  elements={x['name']}  parameters={params_list}\n")
-
+                            # handling html tags not found errors.
+                        try:
+                            f_a.write(f'{str(i.text).strip()}\n')
+                            elems_logging_list.append(f"[{req_type}] [{code}] [Start Time: {start_time}  End Time: {str(datetime.datetime.now())}]  url={url}  elements={x['name']}  parameters={params_list}\n")
+                        except AttributeError:
+                            error_logger(url=url, time=f"Start Time: {start_time}   End Time: {str(datetime.datetime.now())}", status='ERROR', error=f'{i} was not found on the webpage', request_type=req_type)
                     
                 element_logger(elements_logs_list=elems_logging_list)
 
